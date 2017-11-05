0ab94a5a60e7f9038930726730d2cb9ff93e1e06
HideakiTouhara <hideee6723@gmail.com>
2017-11-05 19:53:22 +0900

    Handle tap

--- Whack a jellyfish/ViewController.swift
+++ Whack a jellyfish/ViewController.swift
@@ -18,14 +18,39 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.SceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         self.SceneView.session.run(configuration)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.SceneView.addGestureRecognizer(tapGestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func addNode() {
+        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
+        node.position = SCNVector3(0,0,-1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        self.SceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let sceneViewTappedOn = sender.view as! ARSCNView
+        let touchCoordinates = sender.location(in: sceneViewTappedOn)
+        let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
+        if hitTest.isEmpty {
+            
+        } else {
+            let results = hitTest.first!
+            let geometry = results.node.geometry
+            print(geometry)
+        }
+        
+    }
 
     @IBAction func play(_ sender: UIButton) {
+        self.addNode()
     }
     
     @IBAction func reset(_ sender: UIButton) {
